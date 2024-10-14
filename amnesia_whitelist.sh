c7d22c6b7e1e33e814fba37af43a689f33b6ec15
Abysmalduck <unitu696@gmail.com>
2024-10-14 20:37:43 +0300

    Update amnesia_whitelist.sh

--- amnesia_whitelist.sh
+++ amnesia_whitelist.sh
@@ -2,21 +2,21 @@
 
 echo "Amnesia VPN white list v0.1"
 
-sudo mkdir /sys/fs/cgroup/amnesia_whitelist/
+sudo mkdir /sys/fs/cgroup/amnesia_whitelist/ #Creating cgroup
 
-sudo iptables -t mangle -F
+sudo iptables -t mangle -F #mark all packet that located in cgroup
 sudo iptables -t mangle -A OUTPUT -m cgroup --path "amnesia_whitelist" -j MARK --set-mark 171
 sudo iptables -t mangle -A INPUT -m cgroup --path "amnesia_whitelist" -j MARK --set-mark 171
 #sudo iptables -t mangle -A OUTPUT -m cgroup --path "amnesia_whitelist" -j LOG --log-prefix "MARK PACKET IN "
 #sudo iptables -t mangle -A OUTPUT -m cgroup --path "amnesia_whitelist" -j LOG --log-prefix "MARK PACKET OUT "
 
-sudo ip route del "0.0.0.0/1"
+sudo ip route del "0.0.0.0/1" # unroute existing VPN connection 
 sudo ip route del "1.0.0.1"
 sudo ip route del "1.1.1.1"
 sudo ip route del "128.0.0.0/1"
 sudo ip route del "10.33.0.0/24" #is this adress for everyone?
 
-sudo mkdir /etc/iproute2
+sudo mkdir /etc/iproute2 # route our applications to VPN
 sudo echo "200 vpn" | sudo tee -a /etc/iproute2/rt_tables
 sudo ip route add default via 10.33.0.2 dev tun2 table vpn 
 sudo ip rule add fwmark 171 table vpn
@@ -27,6 +27,7 @@ PROCESSES="vesktop discord vesktop.bin firefox"
 
 proc_array=(${PROCESSES//;/ })
 
+#adding applications to cgroup
 while true
 do
     for proc in ${proc_array[@]}; do
