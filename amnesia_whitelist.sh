bb8f874738b6dfa4362c1d89042af312444de609
tdehtyar <unitu696@gmail.com>
2024-10-16 20:34:17 +0300

    Revert "Update amnesia_whitelist.sh"
    This reverts commit c2463104b1d4db086ddcd46fc73647c279b5bedb.
    
    	modified:   amnesia_whitelist.sh

--- amnesia_whitelist.sh
+++ amnesia_whitelist.sh
@@ -17,22 +17,22 @@ do
     sleep 1
 done
 
-mkdir /sys/fs/cgroup/amnesia_whitelist/ #Creating cgroup
+sudo mkdir /sys/fs/cgroup/amnesia_whitelist/ #Creating cgroup
 
-iptables -t mangle -F #mark all packet that located in cgroup
-iptables -t mangle -A OUTPUT -m cgroup --path "amnesia_whitelist" -j MARK --set-mark 171
-iptables -t mangle -A INPUT -m cgroup --path "amnesia_whitelist" -j MARK --set-mark 171
+sudo iptables -t mangle -F #mark all packet that located in cgroup
+sudo iptables -t mangle -A OUTPUT -m cgroup --path "amnesia_whitelist" -j MARK --set-mark 171
+sudo iptables -t mangle -A INPUT -m cgroup --path "amnesia_whitelist" -j MARK --set-mark 171
 
-ip route del "0.0.0.0/1" # unroute existing VPN connection
-ip route del "1.0.0.1"
-ip route del "1.1.1.1"
-ip route del "128.0.0.0/1"
-ip route del "10.33.0.0/24" #is this adress for everyone?
+sudo ip route del "0.0.0.0/1" # unroute existing VPN connection 
+sudo ip route del "1.0.0.1"
+sudo ip route del "1.1.1.1"
+sudo ip route del "128.0.0.0/1"
+sudo ip route del "10.33.0.0/24" #is this adress for everyone?
 
-mkdir /etc/iproute2 # route our applications to VPN
-echo "200 vpn" |  tee -a /etc/iproute2/rt_tables
-ip route add default via 10.33.0.2 dev tun2 table vpn
-ip rule add fwmark 171 table vpn
+sudo mkdir /etc/iproute2 # route our applications to VPN
+sudo echo "200 vpn" | sudo tee -a /etc/iproute2/rt_tables
+sudo ip route add default via 10.33.0.2 dev tun2 table vpn 
+sudo ip rule add fwmark 171 table vpn
 
 echo "Staring to poll processes"
 
@@ -47,7 +47,7 @@ do
         pids=$(pidof $proc)
         pids_array=(${pids//;/ })
         for el in ${pids_array[@]}; do
-            echo ${el} |  tee -a /sys/fs/cgroup/amnesia_whitelist/cgroup.procs > /dev/null
+            sudo echo ${el} | sudo tee -a /sys/fs/cgroup/amnesia_whitelist/cgroup.procs > /dev/null
         done
     done
     sleep 1
